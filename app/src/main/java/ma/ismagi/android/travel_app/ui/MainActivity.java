28894416a8a70e9c8a3321f75f715306903a80d0
Anas Bensaid <bensaidanass9@gmail.com>
2023-05-13 14:48:25 +0100

    Initial Commit

---
+++ app/src/main/java/ma/ismagi/android/travel_app/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package ma.ismagi.android.travel_app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ma.ismagi.android.travel_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
